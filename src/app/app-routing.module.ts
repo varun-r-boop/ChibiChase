90f22e13a40bccbc17ff47d913a9af6ec7914b62
varun-r-boop <varunpanthom6555@gmail.com>
2023-10-14 10:46:46 +0530

    creating home and topbar component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './main/home/home.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {
+    path: "home",
+    component:HomeComponent,
+    data: {
+      title: "Home",
+    },
+    canActivate: [],
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { TopBarComponent } from './main/top-bar/top-bar.component';
+import { HomeComponent } from './main/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    TopBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
